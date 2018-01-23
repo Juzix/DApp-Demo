648b8067c87446a7fdaddb175613f65ab6dc696c
liuzhiqiang <liuzhiqiang@juzix.io>
2018-01-23 16:27:11 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -9,7 +9,8 @@ module.exports = {
         index: path.resolve(__dirname, '../demo/index.html'),
         assetsRoot: path.resolve(__dirname, '../demo'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/bookMgr',
+        assetsPublicPath: '/',
+        // assetsPublicPath: '/bookMgr',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
